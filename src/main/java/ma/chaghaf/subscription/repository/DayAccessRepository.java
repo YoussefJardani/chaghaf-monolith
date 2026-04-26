c036e613f8b84ad5bb29d8b09b763e4af951b286
Youssef-0811 <youssefardanij@gmail.com>
2026-04-26 14:26:01 +0100

    supprimer le fihcie

--- src/main/java/ma/chaghaf/subscription/repository/DayAccessRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.chaghaf.subscription.repository;
-
-import ma.chaghaf.subscription.entity.DayAccess;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DayAccessRepository extends JpaRepository<DayAccess, Long> {}
